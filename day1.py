35751a09106b7198efabaee40db97b095734eef9
Syed Roman Bin Walid <137637368+CodeWithWalid@users.noreply.github.com>
2024-07-12 20:20:03 +0600

    Create day1.py
    Day 1:  Brand Name Generator

---
+++ day1.py
@@ -0,0 +1,6 @@
+# Brand Name Generator
+print("Welcome to Brand Name Generator")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+band_name = city + " " + pet
+print("Your Brand name is " + band_name)
